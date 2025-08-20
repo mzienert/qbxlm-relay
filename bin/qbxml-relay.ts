c51fce4f5673b7a51826acc85f0fc0261207de1b
Matthew Zienert <mzienert@gmail.com>
2025-08-19 20:52:52 -0600

    stub out project

---
+++ bin/qbxml-relay.ts
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import * as cdk from 'aws-cdk-lib';
+import { QbxmlRelayStack } from '../lib/qbxml-relay-stack';
+
+const app = new cdk.App();
+
+const environment = app.node.tryGetContext('environment') || 'dev';
+
+new QbxmlRelayStack(app, `QbxmlRelayStack-${environment}`, {
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION || 'us-east-1',
+  },
+  environment,
+});
